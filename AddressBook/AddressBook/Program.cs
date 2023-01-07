a2c5900203b997ac47913af4ec06f46bbaed3022
Nitin308 <nitin3081998@gmail.com>
2023-01-07 13:58:51 +0530

    Using Console to Add Person Details

---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,54 @@
+﻿using AddressBook.Model;
+using AddressBook.Repository;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book");
+
+            Console.WriteLine("\nEnter First Name: ");
+            string? firstName = Console.ReadLine();
+
+            Console.WriteLine("\nEnter Last Name: ");
+            string? lastName = Console.ReadLine();
+
+            Console.WriteLine("\nEnter Mobile Number: ");
+            long mobileNumber = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("\nEnter EMail: ");
+            string? eMail = Console.ReadLine();
+
+            Console.WriteLine("\nEnter Address: ");
+            string? address = Console.ReadLine();
+
+            Console.WriteLine("\nEnter City: ");
+            string? city = Console.ReadLine();
+
+            Console.WriteLine("\nEnter State: ");
+            string? state = Console.ReadLine();
+
+            Console.WriteLine("\nEnter Zip: ");
+            int zip = Convert.ToInt32(Console.ReadLine());
+
+            ContactDetails contactDetail = new ContactDetails()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                MobileNumber = mobileNumber,
+                EMail = eMail,
+                Address = address,
+                City = city,
+                State = state,
+                Zip = zip
+            };
+
+            ContactDetailsRepository contactDetails = new ContactDetailsRepository();
+            contactDetails.AddContactDetails(contactDetail);
+            contactDetails.DisplayContact();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AddressBook/AddressBook/Repository/ContactDetailsRepository.cs
@@ -0,0 +1,34 @@
+﻿using AddressBook.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.Repository
+{
+    internal class ContactDetailsRepository
+    {
+        Dictionary<long, ContactDetails> contactDetails = new Dictionary<long, ContactDetails>();
+
+        public void AddContactDetails(ContactDetails contactDetail) //using mobile number because it is unique.
+        {
+            contactDetails.Add(contactDetail.MobileNumber, contactDetail);
+        }
+
+        public void DisplayContact()
+        {
+            foreach (var item in contactDetails)
+            {
+                Console.WriteLine($"\nFirst Name::{item.Value.FirstName}");
+                Console.WriteLine($"Last Name::{item.Value.LastName}");
+                Console.WriteLine($"Mobile Number::{item.Value.MobileNumber}");
+                Console.WriteLine($"EMail::{item.Value.EMail}");
+                Console.WriteLine($"Address::{item.Value.Address}");
+                Console.WriteLine($"City::{item.Value.City}");
+                Console.WriteLine($"State::{item.Value.State}");
+                Console.WriteLine($"Zip::{item.Value.Zip}");
+            }
+        }
+    }
+}
