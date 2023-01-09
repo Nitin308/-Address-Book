0345af7c36bb3e088c8dd9b10882f7a4eecd0c29
Nitin308 <nitin3081998@gmail.com>
2023-01-09 12:50:18 +0530

    Add Address Book IO Operation

---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,41 @@
+﻿using AddressBookSystem_Part2.FileIOOperation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem_Part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\DELL\source\repos\AddressBookSystem_UC13_FileIO\AddressBookSystem_UC13_FileIO\MyFiles\Records.txt";
+
+            PersonInput input = new PersonInput();
+            //Getting details from user
+            Console.WriteLine("\nEnter your First Name : ");
+            input.fName = Console.ReadLine();
+            Console.WriteLine("Enter your Last Name : ");
+            input.lName = Console.ReadLine();
+            Console.WriteLine("Enter your Address : ");
+            input.address = Console.ReadLine();
+            Console.WriteLine("Enter your City Name : ");
+            input.city = Console.ReadLine();
+            Console.WriteLine("Enter your State Name : ");
+            input.state = Console.ReadLine();
+            Console.WriteLine("Enter your Zip Code : ");
+            input.zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Phone Number : ");
+            input.phoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter your Email Address: ");
+            input.email = Console.ReadLine();
+
+            FileIO.WriteRecordsInFile(path, input); // Writing records into file
+            Console.WriteLine("\n\nRecords present in file are : ");
+            FileIO.ReadRecordsFromFile(path); // Reading all records from file 
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AddressBook/FileIOOperation/FileIO.cs
@@ -0,0 +1,45 @@
+﻿namespace AddressBookSystem_Part2.FileIOOperation
+{
+    internal class FileIO
+    {
+        /// <summary>
+        /// Write all the details in File
+        /// </summary>
+        public static void WriteRecordsInFile(string path, PersonInput person)
+        {
+
+            if (File.Exists(path))
+            {
+                StreamWriter sw = File.AppendText(path);
+                sw.WriteLine("\nFirst Name : " + person.fName);
+                sw.WriteLine("Last Name : " + person.lName);
+                sw.WriteLine("Address : " + person.address);
+                sw.WriteLine("City : " + person.city);
+                sw.WriteLine("State : " + person.state);
+                sw.WriteLine("Email : " + person.email);
+                sw.WriteLine("Zip code : " + person.zip);
+                sw.WriteLine();
+                sw.Close();
+                Console.WriteLine("\nData added successfully in file");
+            }
+            else
+            {
+                Console.WriteLine("\nFile Not Found");
+            }
+        }
+        /// <summary>
+        /// Read all records from which are present in Files
+        /// </summary>
+        public static void ReadRecordsFromFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                Console.WriteLine(File.ReadAllText(path));
+            }
+            else
+            {
+                Console.WriteLine("\nFile Not Found");
+            }
+        }
+    }
+}
